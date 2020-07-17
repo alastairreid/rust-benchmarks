f192499c4aa7f1fe8c75bc0f5125e33c73a288ef
Alastair Reid <adreid@google.com>
2020-07-17 12:59:39 +0000

    Refactor klee annotations

---
+++ klee-annotations/src/lib.rs
@@ -0,0 +1,69 @@
+#![cfg_attr(feature = "verifier_panic_handler", feature(panic_info_message))]
+
+use std::os::raw;
+use std::ffi::CString;
+use std::default::Default;
+
+pub fn verifier_assume(cond: bool) {
+    extern "C" { fn klee_assume(cond: usize); }
+    unsafe { klee_assume(if cond {1} else {0}) }
+}
+
+pub fn verifier_verify(cond: bool) {
+    if !cond {
+        verifier_report_error("verification failed")
+    }
+}
+
+pub fn verifier_abstract_value<T: Default>(_t: T) -> T {
+    extern "C" { fn klee_make_symbolic(data: *mut raw::c_void, length: usize, name: *const raw::c_char); }
+
+    let mut r = T::default();
+    unsafe {
+        let data   = std::mem::transmute(&mut r);
+        let length = std::mem::size_of::<T>();
+        let null = 0 as *const i8;
+        klee_make_symbolic(data, length, null)
+    }
+    return r;
+}
+
+extern "C" {
+    fn klee_report_error(file: *const raw::c_char, line: usize, message: *const raw::c_char, suffix: *const raw::c_char) -> !;
+}
+
+pub fn verifier_report_error(message: &str) -> ! {
+    let null = 0 as *const i8;
+    let file = null; // ignored by KLEE
+    let line = 0;    // ignored by KLEE
+    let suffix = ""; // ignored by KLEE
+
+    let message = CString::new(message).unwrap();
+    let suffix  = CString::new(suffix).unwrap();
+    unsafe {
+        klee_report_error(file, line, message.as_ptr(), suffix.as_ptr())
+    }
+}
+
+#[cfg(feature = "verifier_panic_handler")]
+use core::panic::PanicInfo;
+
+#[cfg(feature = "verifier_panic_handler")]
+use std::fmt::Write;
+
+#[cfg(feature = "verifier_panic_handler")]
+fn verifier_panic_hook(info: &PanicInfo) {
+    let mut message = String::new();
+    match info.message() {
+        None => message.write_str("panic"),
+        Some(m) => message.write_fmt(*m)
+    }.unwrap();
+    verifier_report_error(&message)
+}
+
+// Calling this before starting verification ensures that
+// panic messages are displayed by KLEE.
+#[cfg(feature = "verifier_panic_handler")]
+pub fn verifier_set_panic_hook() {
+    std::panic::set_hook(Box::new(verifier_panic_hook))
+}
--- verifier/src/lib.rs
+++ verifier/src/lib.rs
@@ -1,85 +1,8 @@
-#![feature(panic_info_message)]
-
-use core::panic::PanicInfo;
-use std::os::raw;
-use std::ffi::CString;
-use std::default::Default;
-use std::fmt::Write;
-
-pub fn verifier_assume(cond: bool) {
-    extern "C" { fn klee_assume(cond: usize); }
-    unsafe { klee_assume(if cond {1} else {0}) }
-}
-
-pub fn verifier_verify(cond: bool) {
-    if !cond {
-        report_error("verification failed")
-    }
-}
-
-pub fn verifier_abstract_value<T: Default>(_t: T) -> T {
-    extern "C" { fn klee_make_symbolic(data: *mut raw::c_void, length: usize, name: *const raw::c_char); }
-
-    let mut r = T::default();
-    unsafe {
-        let data   = std::mem::transmute(&mut r);
-        let length = std::mem::size_of::<T>();
-        let null = 0 as *const i8;
-        klee_make_symbolic(data, length, null)
-    }
-    return r;
-}
-
-extern "C" {
-    fn klee_report_error(file: *const raw::c_char, line: usize, message: *const raw::c_char, suffix: *const raw::c_char) -> !;
-}
-
-pub fn report_error(message: &str) -> ! {
-    let null = 0 as *const i8;
-    let file = null; // ignored by KLEE
-    let line = 0;    // ignored by KLEE
-    let suffix = ""; // ignored by KLEE
-
-    unsafe {
-        klee_report_error(file,
-                          line,
-                          CString::new(message).unwrap().as_ptr(),
-                          CString::new(suffix).unwrap().as_ptr())
-    }
-}
-
-fn panic_hook(info: &PanicInfo) {
-    let mut msg = String::new();
-    match info.message() {
-        None => msg.write_str("panic"),
-        Some(m) => msg.write_fmt(*m)
-    }.unwrap();
-
-    let null = 0 as *const i8;
-    let file = null; // ignored by KLEE
-    let line = 0;    // ignored by KLEE
-    let suffix = ""; // ignored by KLEE
-
-    unsafe {
-        klee_report_error(file,
-                          line,
-                          CString::new(msg).unwrap().as_ptr(),
-                          CString::new(suffix).unwrap().as_ptr())
-    }
-}
-
-// Calling this before starting verification ensures that
-// panic messages are displayed by KLEE.
-pub fn set_panic_hook() {
-    std::panic::set_hook(Box::new(panic_hook))
-}
-
-
 #[macro_export]
 macro_rules! assume {
     ($condition:expr) => {
         if cfg!(feature = "verifier-klee") {
-            $crate::verifier_assume($condition)
+            klee_annotations::verifier_assume($condition)
         } else {
             assert!($condition);
         }
@@ -90,7 +13,7 @@ macro_rules! assume {
 macro_rules! assert {
     ($condition:expr) => {
         if cfg!(feature = "verifier-klee") {
-            $crate::verifier_verify($condition)
+            klee_annotations::verifier_verify($condition)
         } else {
             assert!($condition);
         }
@@ -101,7 +24,7 @@ macro_rules! assert {
 macro_rules! assert_eq {
     ($left:expr, $right:expr) => (
         if cfg!(feature = "verifier-klee") {
-            $crate::verifier_verify($left == $right)
+            klee_annotations::verifier_verify($left == $right)
         } else {
             assert_eq!($left, $right);
         }
@@ -112,7 +35,7 @@ macro_rules! assert_eq {
 macro_rules! assert_ne {
     ($left:expr, $right:expr) => (
         if cfg!(feature = "verifier-klee") {
-            $crate::verifier_verify($left != $right)
+            klee_annotations::verifier_verify($left != $right)
         } else {
             assert!($left != $right);
         }
@@ -123,7 +46,7 @@ macro_rules! assert_ne {
 macro_rules! unreachable {
     () => (
         if cfg!(feature = "verifier-klee") {
-            $crate::report_error("unreachable");
+            klee_annotations::verifier_report_error("unreachable");
         } else {
             unreachable!();
         }
@@ -134,7 +57,7 @@ macro_rules! unreachable {
 macro_rules! nondet {
     ($value:expr) => {
         if cfg!(feature = "verifier-klee") {
-            $crate::verifier_abstract_value($value)
+            klee_annotations::verifier_abstract_value($value)
         } else {
             $value
         }
