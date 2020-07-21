93d2c06ac88e9c971158f364a84ac5285102a072
Alastair Reid <adreid@google.com>
2020-07-21 16:56:41 +0000

    Added more symbolic tests
    This showed that the Vec implementation of Symbolic is unusable

--- benchmarks/smack-regressions/symbolic/arith.rs
+++ benchmarks/smack-regressions/symbolic/arith.rs
@@ -5,6 +5,8 @@ pub fn main() {
     test1();
     test2();
     test3();
+    test4();
+    test5();
 }
 
 pub fn test1() {
@@ -29,4 +31,20 @@ pub fn test3() {
     verifier::assume!(std::i32::MIN / 2 <= b);
     verifier::assume!(b <= std::i32::MAX / 2);
     verifier::assert_eq!(a + b, b + a);
+}
+
+pub fn test4() {
+    // symbolic tuples
+    let (a, b): (u32, u32) = symbolic::Symbolic::symbolic();
+    verifier::assert!(a <= b || b <= a);
+}
+
+pub fn test5() {
+    // symbolic vectors - this check blows up at the moment
+    klee_annotations::verifier_reject(); // abort this test - otherwise KLEE times out
+    let v: Vec<u32> = symbolic::Symbolic::symbolic();
+    verifier::assume!(v.len() == 0);
+    for x in &v {
+        verifier::assert!(*x == std::u32::MAX || *x + 1 != *x);
+    }
 }
